259e6e2cbc345eb64a540944f7fdd1af3e680f24
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-26 10:20:07 +0900

    HomeFragment

---
+++ presentation/src/main/java/com/example/aichatbot_android/ui/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.aichatbot_android.ui.home
+
+import com.example.aichatbot_android.R
+import com.example.aichatbot_android.databinding.FragmentHomeBinding
+import com.example.aichatbot_android.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun init() {
+
+    }
+}
