5b0fb9f8419ce56677d599101aab43ffabf81c0b
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-26 10:20:19 +0900

    Change MainActivity

---
+++ presentation/src/main/java/com/example/aichatbot_android/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.aichatbot_android.ui
+
+import com.example.aichatbot_android.R
+import com.example.aichatbot_android.databinding.ActivityMainBinding
+import com.example.aichatbot_android.ui.base.BaseActivity
+
+class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+
+    override fun observe() {
+    }
+}
