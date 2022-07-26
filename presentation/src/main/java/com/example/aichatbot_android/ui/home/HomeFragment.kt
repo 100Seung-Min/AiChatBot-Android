512dedd476c9f459f36c12d4c7f33b3e0344491b
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-26 10:50:14 +0900

    navigationGraph

--- presentation/src/main/java/com/example/aichatbot_android/ui/home/HomeFragment.kt
+++ presentation/src/main/java/com/example/aichatbot_android/ui/home/HomeFragment.kt
@@ -1,11 +1,17 @@
 package com.example.aichatbot_android.ui.home
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.aichatbot_android.R
 import com.example.aichatbot_android.databinding.FragmentHomeBinding
 import com.example.aichatbot_android.ui.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun init() {
+        binding.homeFragment = this
+    }
 
+    fun moveChat(view: View) {
+        this.findNavController().navigate(R.id.action_homeFragment_to_chatFragment)
     }
 }
