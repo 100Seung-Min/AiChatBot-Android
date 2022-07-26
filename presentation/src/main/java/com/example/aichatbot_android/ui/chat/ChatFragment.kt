fdd9df58615f1fd1c8f454540e7728ab3823bfed
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-26 10:19:58 +0900

    ChatFragment

---
+++ presentation/src/main/java/com/example/aichatbot_android/ui/chat/ChatFragment.kt
@@ -0,0 +1,11 @@
+package com.example.aichatbot_android.ui.chat
+
+import com.example.aichatbot_android.R
+import com.example.aichatbot_android.databinding.FragmentChatBinding
+import com.example.aichatbot_android.ui.base.BaseFragment
+
+class ChatFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat) {
+    override fun init() {
+
+    }
+}
