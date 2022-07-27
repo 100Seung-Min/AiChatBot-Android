d4a91744a4d19efeec17c770277595dc2131a8f2
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-27 22:31:37 +0900

    backAction

--- presentation/src/main/java/com/example/aichatbot_android/ui/chat/ChatFragment.kt
+++ presentation/src/main/java/com/example/aichatbot_android/ui/chat/ChatFragment.kt
@@ -1,11 +1,17 @@
 package com.example.aichatbot_android.ui.chat
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.aichatbot_android.R
 import com.example.aichatbot_android.databinding.FragmentChatBinding
 import com.example.aichatbot_android.ui.base.BaseFragment
 
 class ChatFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat) {
     override fun init() {
+        binding.chatFragment = this
+    }
 
+    fun backBtn(view: View) {
+        findNavController().popBackStack()
     }
 }
