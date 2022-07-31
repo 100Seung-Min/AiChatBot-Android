3c0b92b423a5313a92d212ece46328a683c3f87f
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-31 22:53:15 +0900

    Wiki Request/Response Change

--- data/src/main/java/com/example/data/remote/request/WikiRequest.kt
+++ data/src/main/java/com/example/data/remote/request/WikiRequest.kt
@@ -1,17 +1,28 @@
 package com.example.data.remote.request
 
+import com.example.domain.param.WikiParam
 import com.google.gson.annotations.SerializedName
 
 data class WikiRequest(
     @SerializedName("access_key")
-    val access_key: String = "24049d6a-b054-4a8d-909d-af913c5e2bba",
+    val access_key: String,
     @SerializedName("argument")
     val argument: Question
+) {
+    data class Question(
+        @SerializedName("question")
+        val question: String,
+        @SerializedName("type")
+        val type: String
+    )
+}
+
+fun WikiParam.toRequest() = WikiRequest(
+    access_key = access_key,
+    argument = argument.toRequest()
 )
 
-data class Question(
-    @SerializedName("question")
-    val question: String,
-    @SerializedName("type")
-    val type: String
+fun WikiParam.Question.toRequest() = WikiRequest.Question(
+    question = question,
+    type = type
 )
--- data/src/main/java/com/example/data/remote/response/WikiResponse.kt
+++ data/src/main/java/com/example/data/remote/response/WikiResponse.kt
@@ -1,5 +1,6 @@
 package com.example.data.remote.response
 
+import com.example.domain.entity.WikiEntity
 import com.google.gson.annotations.SerializedName
 
 data class WikiResponse(
@@ -7,36 +8,58 @@ data class WikiResponse(
     val result: Int,
     @SerializedName("return_object")
     val returnObject: ReturnObject
-)
+) {
+    data class ReturnObject(
+        @SerializedName("WiKiInfo")
+        val wiKiInfo: WiKiInfo
+    ) {
+        data class WiKiInfo(
+            @SerializedName("IRInfo")
+            val iRInfo: List<IRInfo>,
+            @SerializedName("AnswerInfo")
+            val answerInfo: List<AnswerInfo>
+        ) {
+            data class IRInfo(
+                @SerializedName("wiki_title")
+                val wikiTitle: String,
+                @SerializedName("sent")
+                val sent: String,
+                @SerializedName("url")
+                val url: String
+            )
+            fun IRInfo.toEntity() = WikiEntity.ReturnObject.WiKiInfo.IRInfo(
+                wikiTitle = wikiTitle,
+                sent = sent,
+                url = url
+            )
 
-data class ReturnObject(
-    @SerializedName("WiKiInfo")
-    val wiKiInfo: WiKiInfo
-)
-
-data class WiKiInfo(
-    @SerializedName("IRInfo")
-    val iRInfo: List<IRInfo>,
-    @SerializedName("AnswerInfo")
-    val answerInfo: List<AnswerInfo>
-)
-
-data class IRInfo(
-    @SerializedName("wiki_title")
-    val wikiTitle: String,
-    @SerializedName("sent")
-    val sent: String,
-    @SerializedName("url")
-    val url: String
-)
-
-data class AnswerInfo(
-    @SerializedName("rank")
-    val rank: Float,
-    @SerializedName("answer")
-    val answer: String,
-    @SerializedName("confidence")
-    val confidence: Float,
-    @SerializedName("url")
-    val url: List<String>
+            data class AnswerInfo(
+                @SerializedName("rank")
+                val rank: Float,
+                @SerializedName("answer")
+                val answer: String,
+                @SerializedName("confidence")
+                val confidence: Float,
+                @SerializedName("url")
+                val url: List<String>
+            )
+            fun AnswerInfo.toEntity() = WikiEntity.ReturnObject.WiKiInfo.AnswerInfo(
+                rank = rank,
+                answer = answer,
+                confidence = confidence,
+                url = url
+            )
+        }
+        fun WiKiInfo.toEntity() = WikiEntity.ReturnObject.WiKiInfo(
+            iRInfo = iRInfo.map { it.toEntity() },
+            answerInfo = answerInfo.map { it.toEntity() }
+        )
+    }
+    fun ReturnObject.toEntity() = WikiEntity.ReturnObject(
+        wiKiInfo = wiKiInfo.toEntity()
+    )
+}
+fun WikiResponse.toEntity() = WikiEntity(
+    result = result,
+    returnObject = returnObject.toEntity()
 )
