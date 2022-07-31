b2c289708478f8245c31894d43fcdd9b09033c8f
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-31 23:04:17 +0900

    add provideWikiRepository

--- presentation/src/main/java/com/example/aichatbot_android/di/RepositoryModule.kt
+++ presentation/src/main/java/com/example/aichatbot_android/di/RepositoryModule.kt
@@ -1,10 +1,18 @@
 package com.example.aichatbot_android.di
 
+import com.example.data.remote.datasource.WikiDataSource
+import com.example.data.repository.WikiRepositoryImpl
+import com.example.domain.repository.WikiRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideWikiRepository(dataSource: WikiDataSource): WikiRepository = WikiRepositoryImpl(dataSource)
 }
