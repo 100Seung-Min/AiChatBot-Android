15be1e963bcfdd8d47c089eadceef1d9b293b934
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-07-31 21:14:22 +0900

    NetworkModule Setting

--- presentation/src/main/java/com/example/aichatbot_android/di/NetworkModule.kt
+++ presentation/src/main/java/com/example/aichatbot_android/di/NetworkModule.kt
@@ -1,10 +1,48 @@
 package com.example.aichatbot_android.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private const val BaseURL = "http://aiopen.etri.re.kr:8000/"
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BaseURL)
+            .client(okHttpClient)
+            .client(provideHttpClient())
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
 }
