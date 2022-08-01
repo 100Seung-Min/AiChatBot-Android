157992c0ed4df50d84e5d4c3b6791f6167666c56
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-02 00:16:56 +0900

    LawAPI Entity/Param

---
+++ domain/src/main/java/com/example/domain/entity/LawEntity.kt
@@ -0,0 +1,23 @@
+package com.example.domain.entity
+
+data class LawEntity(
+    val result: Int,
+    val returnObject: ReturnObject
+) {
+    data class ReturnObject(
+        val legalInfo: LegalInfo
+    ) {
+        data class LegalInfo(
+            val answerInfo: List<AnswerInfo>,
+            val RelatedQs: List<String>
+        ) {
+            data class AnswerInfo(
+                val rank: Float,
+                val answer: String,
+                val source: String,
+                val clause: String,
+                val confidence: Float
+            )
+        }
+    }
+}
---
+++ domain/src/main/java/com/example/domain/param/LawParam.kt
@@ -0,0 +1,11 @@
+package com.example.domain.param
+
+data class LawParam(
+    val access_key: String,
+    val argument: Argument
+) {
+    data class Argument(
+        val question: String,
+        val type: String
+    )
+}
