d425e9096952d73292672f0657016fd27152444d
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-01 12:03:21 +0900

    editText / TextWatcher

--- presentation/src/main/java/com/example/aichatbot_android/ui/chat/ChatFragment.kt
+++ presentation/src/main/java/com/example/aichatbot_android/ui/chat/ChatFragment.kt
@@ -1,6 +1,10 @@
 package com.example.aichatbot_android.ui.chat
 
+import android.graphics.Color
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.aichatbot_android.R
@@ -17,5 +21,16 @@ class ChatFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat) {
     override fun init() {
         binding.chatFragment = this
         binding.viewModel = viewModel
+        textWriting()
+    }
+
+    private fun textWriting() = with(binding){
+        userChatTxt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+            override fun afterTextChanged(p0: Editable?) {
+                sendBtn.setTextColor(Color.parseColor(if(p0.toString().isNullOrEmpty()) "#B4B4B4" else "#3182F7"))
+            }
+        })
     }
 }
