d320b3fa304c185e53d45d4ac44edcc051404081
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-01 23:49:24 +0900

    action sendBtnClick

--- presentation/src/main/java/com/example/aichatbot_android/ui/chat/ChatFragment.kt
+++ presentation/src/main/java/com/example/aichatbot_android/ui/chat/ChatFragment.kt
@@ -4,12 +4,11 @@ import android.graphics.Color
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import com.example.aichatbot_android.R
 import com.example.aichatbot_android.databinding.FragmentChatBinding
 import com.example.aichatbot_android.ui.base.BaseFragment
+import com.example.aichatbot_android.ui.extension.hide
 import com.example.aichatbot_android.viewmodel.ChatViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,12 +23,25 @@ class ChatFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat) {
         textWriting()
     }
 
-    private fun textWriting() = with(binding){
+    fun sendBtn(view: View) = with(binding.userChatTxt) {
+        if (text.toString().isNullOrEmpty()) {
+            return shortToast("질문을 입력해주세요")
+        }
+        when (viewModel.chatType.value) {
+            "위키박사" -> viewModel.wikiQA(text.toString())
+        }
+        text.clear()
+        hide()
+    }
+
+    private fun textWriting() = with(binding) {
         userChatTxt.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
             override fun afterTextChanged(p0: Editable?) {
-                sendBtn.setTextColor(Color.parseColor(if(p0.toString().isNullOrEmpty()) "#B4B4B4" else "#3182F7"))
+                sendBtn.setTextColor(Color.parseColor(if (userChatTxt.text.toString().isNullOrEmpty()) "#B4B4B4" else "#3182F7"))
             }
         })
     }
