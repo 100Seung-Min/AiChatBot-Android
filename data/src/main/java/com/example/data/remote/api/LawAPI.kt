6e136e1b9bcc36f0061fff3facc5cd1cbed61da3
100Seung-Min <81063667+100Seung-Min@users.noreply.github.com>
2022-08-02 00:10:44 +0900

    WikiRequest -> LawRequest

--- data/src/main/java/com/example/data/remote/api/LawAPI.kt
+++ data/src/main/java/com/example/data/remote/api/LawAPI.kt
@@ -1,13 +1,13 @@
 package com.example.data.remote.api
 
-import com.example.data.remote.request.WikiRequest
+import com.example.data.remote.request.LawRequest
 import com.example.data.remote.response.LawResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface LawAPI {
     @POST("LegalQA")
-    suspend fun LawQA(
-        @Body lowRequest: WikiRequest
+    suspend fun lawQA(
+        @Body lowRequest: LawRequest
     ): LawResponse
 }
